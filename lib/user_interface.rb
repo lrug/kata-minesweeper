783a11f99f73df21a6af8da434c275e9196a342e
Tom ten Thij <git@tomtenthij.nl>
2009-09-09 19:26:00 +0100

    iteration 2 tests passing.

--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -25,6 +25,7 @@ class UserInterface
       
       opts.on("--size=[width,height]", :REQUIRED, Array) do |size|
         @size = size
+        @mines = []
       end
       
       opts.on("--mines=[[x,y],[x',y'], ... ]", Array) do |mines|
@@ -34,6 +35,35 @@ class UserInterface
   end
 
   def print_revealed_board
-    puts ".....\n.....\n.....\n"
+    @size[1].times do |y|
+      @size[0].times do |x|
+        render_cell (x,y)
+      end
+      puts
+    end
+  end
+
+  def render_cell (x,y)
+    if(mine_at(x,y)) 
+      print "*"
+    else
+      print neighbours(x,y)
+    end
+  end
+  
+  def neighbours(x,y)
+    if @mines.size == 0
+      '.'
+    else
+      1
+    end
+  end
+  
+  def count_neighbours(x,y)
+    @counts[x][y]
+  end
+  
+  def mine_at(x,y)
+    @mines.include?([x+1,y+1])
   end
 end
